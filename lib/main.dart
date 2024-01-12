cc275bc7df1cd7427cae39ecd987b8108ee25f63
Aayush Chaube <chaubeaayush@gmail.com>
2024-01-13 03:54:22 +0530

    Removed debug banner..

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(
         useMaterial3: true,
       ).copyWith(
